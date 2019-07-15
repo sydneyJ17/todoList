5311e47068eeb414143e0cc54696f014b9ee143f
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-15 11:50:54 -0500

    iteration 0

---
+++ todoList/TableViewController.swift
@@ -0,0 +1,60 @@
+
+import UIKit
+
+class tableViewControllerTableViewController: UITableViewController {
+    
+    
+    func createToDos() -> [ToDo] {
+        
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        // important is set to false by default
+        
+        return [swift, dog]
+    }
+    
+    var toDos : [ToDo] = []
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+        
+        
+    }
+    
+    
+    /*override func numberOfSections(in tableView: UITableView) -> Int {
+     // #warning Incomplete implementation, return the number of sections
+     return 0
+     }*/
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+    
+    
+}
---
+++ todoList/Todo.swift
@@ -0,0 +1,23 @@
+//
+//  Todo.swift
+//  todoList
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+    
+    
+    
+    
+    
+    
+}
+
+
